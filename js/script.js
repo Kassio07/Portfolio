1dc683d13c2ef5d22a4b0d49e473f9415d632818
Kassio Kennedy <154475006+Kassio07@users.noreply.github.com>
2025-01-19 21:32:28 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -4,17 +4,31 @@ let hamburguerOn = document.querySelector(".fa-solid.fa-bars");
 let hamburguerOff = document.querySelector('.fa-solid.fa-xmark');
 
 
-hamburguerOn.addEventListener("click", ()=>{
-    if(hamburguerOn){
+hamburguerOn.addEventListener("click", () => {
+    if (hamburguerOn) {
         hamburguerOn.style.display = 'none';
         hamburguerOff.style.display = 'block';
         menu.style.display = 'block';
     }
 });
-hamburguerOff.addEventListener("click", ()=>{
-    if(hamburguerOff){
+hamburguerOff.addEventListener("click", () => {
+    if (hamburguerOff) {
         hamburguerOff.style.display = 'none';
         hamburguerOn.style.display = 'block';
         menu.style.display = 'none';
     }
 });
+
+//dropdown-content, evento do click off
+
+// Seleciona todos os link da página
+let links = document.querySelectorAll("a");
+
+// Adiciona um evento de click em cada link
+links.forEach(link => {
+    link.addEventListener("click", (event) => {
+        event.preventDefault();
+    });
+});
+
+
