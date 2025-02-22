3431354d9db4b4f17ff402e9a191cc945b4a1440
Kassio07 <kaesscode@gmail.com>
2025-02-22 13:54:55 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -2,80 +2,85 @@
 AOS.init();
 
 // Seleciona os botões de abrir e fechar o menu
-let menu = document.querySelector('header');
+let menu = document.querySelector("header");
 let hamburguerOn = document.querySelector(".fa-solid.fa-bars");
-let hamburguerOff = document.querySelector('.fa-solid.fa-xmark');
+let hamburguerOff = document.querySelector(".fa-solid.fa-xmark");
 
 // Adiciona um evento de click no botão de abrir o menu - Mobile
 hamburguerOn.addEventListener("click", () => {
-    if (hamburguerOn) {
-        hamburguerOn.style.display = 'none';
-        hamburguerOff.style.display = 'block';
-        menu.style.display = 'block';
-    }
+  if (hamburguerOn) {
+    hamburguerOn.style.display = "none";
+    hamburguerOff.style.display = "block";
+    menu.style.display = "block";
+  }
 });
 hamburguerOff.addEventListener("click", () => {
-    if (hamburguerOff) {
-        hamburguerOff.style.display = 'none';
-        hamburguerOn.style.display = 'block';
-        menu.style.display = 'none';
-    }
+  if (hamburguerOff) {
+    hamburguerOff.style.display = "none";
+    hamburguerOn.style.display = "block";
+    menu.style.display = "none";
+  }
 });
 
 // Accordions Skills
 function toggleAccordeon(index) {
-    // Selecionando os controles
-    const contents = document.querySelectorAll('.accordion-content');
-    const iconAcco = document.querySelectorAll('.accordion-header i');
+  // Selecionando os controles
+  const contents = document.querySelectorAll(".accordion-content");
+  const iconAcco = document.querySelectorAll(".accordion-header i");
 
-    // Accordion 
-    contents.forEach((content, i) => {
-        if (i === index) {
-            content.classList.toggle('show');
-        } else {
-            content.classList.remove('show');
-        }
-    });
-    // Accordion arrow
-    iconAcco.forEach((cont, indice) => {
-        if (indice === index) {
-            cont.classList.toggle('animation');
-        } else {
-            cont.classList.remove('animation');
-        }
-    });
+  // Accordion
+  contents.forEach((content, i) => {
+    if (i === index) {
+      content.classList.toggle("show");
+    } else {
+      content.classList.remove("show");
+    }
+  });
+  // Accordion arrow
+  iconAcco.forEach((cont, indice) => {
+    if (indice === index) {
+      cont.classList.toggle("animation");
+    } else {
+      cont.classList.remove("animation");
+    }
+  });
 }
 
 // Cancela o evento padrão do click
-let links = document.querySelectorAll('.accordion-header');
+let links = document.querySelectorAll(".accordion-header");
 links.forEach((link) => {
-    link.addEventListener('click', (e) => {
-        e.preventDefault();
-    });
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+  });
 });
-
 
 // Evento na sessão de portfólio - evento de click e mostra os itens relacionado
 
 // selecionando os controles
-document.querySelectorAll(".link").forEach(link =>{
-    // Quando clicar no botão faz esse evento
-    link.addEventListener("click", function(event){
-        event.preventDefault(); // Evita o evento padrão do click
+document.querySelectorAll(".link").forEach((link) => {
+  // Quando clicar no botão faz esse evento
+  link.addEventListener("click", function (event) {
+    event.preventDefault(); // Evita o evento padrão do click
 
-        let target = this.getAttribute("data-target");  // seleciona o alvo do click e pega o atributo
+    let target = this.getAttribute("data-target"); // seleciona o alvo do click e pega o atributo
+    let selecaoAlvo = document.getElementById(target); // Seleciona o elemento uma única vez
 
-        // Esconde todos os conteúdos
-        document.querySelectorAll('.port-galery-container').forEach(div =>{
-            div.style.display = "none";
-        })
-        // pega o alvo do click e coloca como display:flex
-        if(target){
-            document.getElementById(target).style.opacity = "0";
-            document.getElementById(target).style.display = "flex";
-            setTimeout(()=>{
-                document.getElementById(target).style.opacity = "1";
-            },300);
-        }
+    //Caso o elemento não exista
+    if (!selecaoAlvo) {
+      return;
+    }
+
+    // Esconde todos os conteúdos
+    document.querySelectorAll(".port-galery-container").forEach((div) => {
+      div.style.display = "none";
+      div.style.opacity = "0";
     });
-})
+
+    //Exibe o elemento alvo com a transição suave
+    selecaoAlvo.style.opacity = '0';
+    selecaoAlvo.style.display = "flex";
+    setTimeout(() => {
+      selecaoAlvo.style.opacity = "1";
+    }, 300);
+  });
+});
